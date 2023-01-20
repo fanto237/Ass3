b0a24c6954d7d31b8afaa5c7dd7d0deb0c0fc045
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-01-20 10:49:32 +0100

    add running rules

---
+++ HowTo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo ""
+echo  -e "\e[32mFür die Komprimierung:\e[0m"
+echo "Geben sie bitte eine Datei als argument ein."
+echo  -e "\e[32mBeispiel:\e[0m ./AUSGABE text.txt"
+echo ""
+
+echo -e "\e[32mFür die Dekomprimierung:\e[0m"
+echo "Geben sie bitte eine bereits komprimierte .bin-Datei als argument ein"
+echo  -e "\e[32mBeispiel:\e[0m ./AUSGABE text.txt.bin"
+echo ""
+
+echo -e "\e[32mFür den Vergleich mit Diff\e[0m"
+echo "Geben sie bitte zwei komprimierten Dateien (.bin) als argument ein"
+echo  -e "\e[32mBeispiel:\e[0m ./AUSGABE text1.txt.bin text2.txt.bin"
